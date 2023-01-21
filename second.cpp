5657b3e411de2c4c289aea774fb1f9a59e2ba5c3
Ali <93874862+ariaksakal@users.noreply.github.com>
2023-01-22 01:49:09 +0300

    Quiz Game
    It's working on Clion but it's not working DevC++ probably because of the year

---
+++ second.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main() {
+
+    char answer;
+    int start;
+    int total;
+    int correct;
+    int false;
+
+    printf("HEY WELCOME TO QUIZ GAME FOR START PRESS 1\n");
+    printf("For exit press -1");
+    scanf("%d", &start);
+    if (start == -1) {
+        return 0;
+    }
+    while (start < 3) {
+        printf("Which year is berserk 1st chapter released?\n");
+        printf("A)1989\nB)1990\nC)1991\nD)1988");
+        scanf(" %c", &answer);
+        if (answer == 'A') {
+            printf("Correct");
+            total += 10;
+        } else {
+            printf("Wrong");
+            total += 0;
+        }
+        printf("Which year is google invented?\n");
+        printf("A)1989\nB)1990\nC)1991\nD)1988");
+        scanf(" %c", &answer);
+        if (answer == 'A') {
+            printf("Correct");
+            total += 10;
+        } else {
+            printf("Wrong");
+            total += 0;
+        }
+        printf("QUIZ IS END YOUR TOTAL SCORE IS %d", total);
+        break;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
