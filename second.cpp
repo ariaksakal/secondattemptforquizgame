521fbf6b15194f73df57c28f96aa0534d642f32b
Ali <93874862+ariaksakal@users.noreply.github.com>
2023-01-22 14:39:20 +0300

    Update second.cpp

--- second.cpp
+++ second.cpp
@@ -1,12 +1,17 @@
 #include <stdio.h>
-
+void pointtable(int correct, int false,int total){
+    printf("Your correct answer is total: %d\nYour total false is; %d\nYour total score is:%d",correct,false,total);
+}
 int main() {
 
     char answer;
     int start;
     int total;
-    int correct;
     int false;
+    int correct;
+
+    correct = 0;
+    false = 0;
 
     printf("HEY WELCOME TO QUIZ GAME FOR START PRESS 1\n");
     printf("For exit press -1");
@@ -19,27 +24,27 @@ int main() {
         printf("A)1989\nB)1990\nC)1991\nD)1988");
         scanf(" %c", &answer);
         if (answer == 'A') {
-            printf("Correct");
+            printf("Correct\n");
             total += 10;
             correct += 1;
         } else {
-            printf("Wrong");
+            printf("Wrong\n");
             total += 0;
-            false += 1;
-        }f
+            false += +1;
+        }
         printf("Which year is google invented?\n");
         printf("A)1989\nB)1990\nC)1991\nD)1988");
         scanf(" %c", &answer);
         if (answer == 'A') {
-            printf("Correct");
+            printf("Correct\n");
             total += 10;
             correct += 1;
         } else {
-            printf("Wrong");
+            printf("Wrong\n");
             total += 0;
             false += 1;
         }
-        printf("QUIZ IS END YOUR TOTAL SCORE IS %d", total);
+        pointtable(correct, false, total);
         break;
 
     }
